58c56cd3e19c6d3f299888c9af3211f5762e8482
Marco <mtroncone17@gmail.com>
2022-12-22 20:23:51 +0100

    Add project files.

---
+++ TestHotReload/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace TestHotReload;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ TestHotReload/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace TestHotReload;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private void NewMethodTest(object sender, EventArgs e)
+    {
+        count+=10;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times. FANTASTIC";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
